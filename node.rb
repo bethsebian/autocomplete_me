3ead3779f5a3258e86244a81622f15cc004f3102
Beth Sebian <esebian@gmail.com>
2015-10-27 14:09:08 -0600

    changed default hash to empty, removed own_letter, and added ability to link to children

--- node.rb
+++ node.rb
@@ -1,28 +1,42 @@
 require 'pry'
 
 class Node
-  attr_accessor :selections, :word
+  attr_accessor :selections, :word, :array, :alphabet_hash
 
   def initialize(array=[])
     @array = array
     @selections = 0
     @word = false
-  end
-
-  def own_letter
-    @array[0]
+    @my_hash = Hash.new
+    create_child(array)
   end
 
   def selections
     @selections
   end
 
+  def create_child(input_array)
+    if input_array.empty?
+    else
+      key = input_array[0]
+      input_array.shift
+      add_key_and_value_to_hash(key, value = Node.new(input_array))
+    end
+  end
+
   def word?
     @word
   end
 
-  def alphabet_hash
-    Hash["a","","b","","c","","d","","e","","f","","g","","h","","i","","j","","k","","l","","m","","n","","o","","p","","q","","r","","s","","t","","u","","v","","w","","x","","y","","z",""]
+  def my_hash
+    @my_hash
   end
 
+  def add_key_and_value_to_hash(key,value)
+    @my_hash[key] = value
+  end
+
+  def link(key)
+    @my_hash[key]
+  end
 end
--- node_test.rb
+++ node_test.rb
@@ -4,11 +4,44 @@ require 'minitest/pride'
 require_relative 'node.rb'
 
 class NodeTest < Minitest::Test
-  def test_new_node_defines_its_own_letter_with_first_letter_from_passed_array
-    word_array = ["t","e","s","t"]
+  def test_new_node_with_empty_array_has_empty_hash
+    word_array = []
     test_node = Node.new(word_array)
 
-    assert_equal "t", test_node.own_letter
+    assert_equal [{}], [test_node.my_hash]
+    assert test_node.my_hash.empty?
+  end
+
+  def test_new_node_with_1_letter_array_creates_key_letter_and_value_new_node
+    word_array = ["e"]
+    test_node = Node.new(word_array)
+
+    assert test_node.link("e").object_id
+    refute test_node.link("a")
+  end
+
+  def test_new_node_with_2_letter_array_creates_1_child_with_1_child
+    word_array = ["f","u"]
+    test_node = Node.new(word_array)
+
+    assert test_node.link("f").object_id
+    assert test_node.link("f").link("u").object_id
+    refute test_node.link("a")
+    assert_equal nil, test_node.link("f").link("g")
+   end
+
+   def test_new_node_with_5_letter_array_creates_5_generations
+    word_array = ["f","u","n","n","y"]
+    test_node = Node.new(word_array)
+
+    assert test_node.link("f").link("u").link("n").link("n").link("y")
+    assert_equal Node, test_node.link("f").class
+    assert_equal Node, test_node.link("f").link("u").class
+    assert_equal Node, test_node.link("f").link("u").link("n").class
+    assert_equal Node, test_node.link("f").link("u").link("n").link("n").class
+    assert_equal Node, test_node.link("f").link("u").link("n").link("n").link("y").class
+    refute test_node.link("f").link("u").link("n").link("n").link("c")
+    refute test_node.link("f").link("g")
   end
 
   def test_it_has_default_selection_value_of_zero
@@ -29,11 +62,6 @@ class NodeTest < Minitest::Test
 
     assert_equal false, test_node.word?
     refute test_node.word?
-  end
-
-  def test_it_is_initialized_with_an_empty_hash_of_alphabet_keys
-    test_node = Node.new(["t","e","s","t"])
-    assert_equal Hash["a","","b","","c","","d","","e","","f","","g","","h","","i","","j","","k","","l","","m","","n","","o","","p","","q","","r","","s","","t","","u","","v","","w","","x","","y","","z",""], test_node.alphabet_hash
   end
 end
 
