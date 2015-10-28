8057c7e38914ad31a3681f14210bc3cbaf55331d
Beth Sebian <esebian@gmail.com>
2015-10-27 21:06:15 -0600

    added insert method on trie

--- complete_me.rb
+++ complete_me.rb
@@ -1,4 +1,13 @@
 require_relative 'node.rb'
 
-class CompleteMe 
+class CompleteMe
+  attr_accessor :root
+
+  def insert(word)
+    if root.nil?
+      @root = Node.new(word.chars)
+    else
+      root.advance(word.chars)
+    end
+  end
 end
--- complete_me_test.rb
+++ complete_me_test.rb
@@ -4,5 +4,66 @@ require 'minitest/pride'
 require_relative 'complete_me.rb'
 
 class CompleteMeTest < Minitest::Test
+
+  def test_it_creates_root_when_first_word_inserted
+    trie = CompleteMe.new
+    trie.insert("")
+
+    assert_equal Node, trie.root.class
+  end
+
+  def test_it_adds_word_to_new_trie
+    trie = CompleteMe.new
+    trie.insert("sit")
+
+    assert trie.root.link("s").link("i").link("t")
+  end
+
+  def test_it_knows_when_a_node_refers_to_a_word
+    trie = CompleteMe.new
+    trie.insert("sit")
+
+    assert trie.root.link("s").link("i").link("t").word?
+    refute trie.root.link("s").link("i").word?
+  end
+
+  def test_it_can_add_a_word_along_an_existing_route
+    trie = CompleteMe.new
+    trie.insert("sofa")
+    trie.insert("so")
+
+    assert trie.root.link("s").link("o").link("f").link("a").word?
+    assert trie.root.link("s").link("o").word?
+    refute trie.root.link("s").word?
+    refute trie.root.link("s").link("o").link("f").word?
+  end
+
+  def test_it_can_add_a_word_along_an_existing_route_and_create_nodes_when_route_ends
+    trie = CompleteMe.new
+    trie.insert("so")
+    trie.insert("sofa")
+
+    assert trie.root.link("s").link("o").link("f").link("a").word?
+    assert trie.root.link("s").link("o").word?
+    refute trie.root.link("s").word?
+    refute trie.root.link("s").link("o").link("f").word?
+  end
+
+  def test_it_can_incorporate_many_words
+    trie = CompleteMe.new
+    trie.insert("so")
+    trie.insert("sofa")
+    trie.insert("banana")
+    trie.insert("mask")
+    trie.insert("hollar")
+    trie.insert("supplies")
+    trie.insert("fauna")
+    trie.insert("umbrella")
+    trie.insert("foxy")
+    trie.insert("ukelele")
+
+    assert_equal ["b", "f", "h", "m", "s", "u"], trie.root.my_hash.keys.sort
+    assert_equal ["k","m"], trie.root.link("u").my_hash.keys.sort
+  end
 end
 
